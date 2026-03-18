05b0e74ef2baf2d3a73ddb31b0da9af98697bf65
Annie <aelizabethm@mun.ca>
2026-03-17 23:08:37 -0230

    remove markers

--- Annie_API/Models/Session.cs
+++ Annie_API/Models/Session.cs
@@ -14,14 +14,6 @@ public class Session
     public DateTime EndTime { get; set; } = DateTime.MinValue;
     public required int Capacity { get; set; }
     public SessionStatus Status { get; set; } = SessionStatus.Available;
-<<<<<<< HEAD
-<<<<<<< HEAD
     public required string Instructor { get; set; }
-=======
-    public required string Instructors { get; set; } = "Default Instructor";
->>>>>>> 7b48f7899f1128450c976cea42a1ca607c41cf5a
-=======
-    public required string Instructors { get; set; } = "Default Instructor";
->>>>>>> 0bd05a2afad5fa38bc7245178b9b62e3abeaa24d
     public string? Location { get; set; }
 }
